9e0b3e55d076b78426bf1350e7163d3e0be6d012
Tom Rijntjes <tom.rijntjes@objectivepartners.com>
2017-12-21 16:54:49 +0100

    lecture 83

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,9 +3,26 @@ defmodule Discuss.TopicController do
   
   alias Discuss.Topic
 
-  def new(conn, params) do
-  	changeset = Topic.changeset(%Topic{},%{})
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render conn, "index.html", topics: topics
+  end
 
-  	render conn, "new.html"
+  def new(conn, _params) do
+  	changeset = Topic.changeset(%Topic{},%{})
+  	render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+  	changeset = Topic.changeset(%Topic{}, topic)
+  	case Repo.insert(changeset) do
+  	  {:ok, post} -> 
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: topic_path(conn, :index))
+  	  {:error, changeset} -> 
+        render conn, "new.html", changeset: changeset
+
+  	end
   end
 end
